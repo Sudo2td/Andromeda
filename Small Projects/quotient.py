fdd6f7ecf66779cb2cf0e581d77108b79d9e0689
sudo2td <sudo2td@gmail.com>
2021-09-02 14:16:58 +0300

    Added quotient V 2.1.0

---
+++ Small Projects/quotient.py
@@ -0,0 +1,6 @@
+print("Find the Quotient of Two Numbers")
+first_number = input("Enter First Number: ")
+second_number = input("Enter Second Number: ")
+
+print(float(first_number) % float(second_number))
+input("Press Enter to Exit: ")
